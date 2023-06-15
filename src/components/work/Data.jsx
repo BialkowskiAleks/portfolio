c0af849aaf90ea52d08936cdfea393753994a82c
Aleks Bialkowski <aleks.bialkowski@gmail.com>
2023-06-15 18:39:17 +0200

    Changes

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,7 +1,3 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
 import Work9 from "../../assets/work9.png";
 
 export const projectsData = [
